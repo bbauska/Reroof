b0b68346e8de9316ed5e69603f7362703d8b3691
Brian Bauska <41387907+bbauska@users.noreply.github.com>
2021-06-18 11:48:18 -0700

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,4 +1,6 @@
-function myfunction()   
+/** Called from index.html within bbauska/CandysRoof.bauska.site/main.  Playing and learning JS.  */
+
+function myfunction()   // write shit.
  {   
 document.write("welcome to Learning from Brian: Javatpoint");  
          } 
