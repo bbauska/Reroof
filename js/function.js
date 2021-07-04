00147af4debf33c66f78afda7f99fc623495bb24
Brian Bauska <41387907+bbauska@users.noreply.github.com>
2021-07-04 12:57:41 -0700

    Update function.js

--- js/function.js
+++ js/function.js
@@ -4,3 +4,4 @@ function myfunction()   // write shit.
  {   
 document.write("welcome to Learning from Brian.  Pick it up as I go along.  Love the shit out of git.");  
          } 
+<a href = "javascript:history.back()">Back to previous page</a>
